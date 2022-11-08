9cab483be1d03719e3476d9b38b0afcfde4f793a
philip12845 <87152429+philip12845@users.noreply.github.com>
2022-11-08 21:58:08 +0000

    Update install.sh
    added bulk of the script other part will just be user choice/options 
    lines 1-46 work for installing the server and having custom names for files

--- install.sh
+++ install.sh
@@ -6,96 +6,87 @@ echo "updated and upgraded all packages"
 
 sudo apt install git && sudo apt install xz-utils
 echo "installed dependencies (git and xz-utils)"
------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# note to self: look at expect program for linux
 
------------------------------------------------------- use this for inspiration 
+echo "welcome to the fivem linux installer..."
 
-#!/usr/bin/env bash
-
-echo "Hello, please introduce yourself."
+echo -n "Your server name: "
+read server_name
+mkdir "$server_name"
+echo "madedir $server_name"
+cd $server_name
+echo "cd into $server_name"
 
 echo -n "Your name: "
-read -r name
+read user_name
+mkdir "$user_name"
+echo "madedir"
+cd $user_name
+echo "cd into $username"
 
-echo "Are you human?"
-
-echo -n "y/n: "
-read -r human
-
-echo "What is your favorite programming language?"
-
-echo -n "Your answer: "
-read -r lang
-
-echo
-echo "Answers:"
-echo "1. $name"
-echo "2. $human"
-echo "3. $lang"
-
-look at expect program for linux
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo "what is your servername"
-inout=$servername
--- allow user to answer and store as variable $servername
-
-echo "what is your username"
-input=$username
--- allow user to answer and store as variable $username
-
-cd /home
-mkdir $username-$servername
-
-
-cd $username-$servername
 echo "start of server install"
-# add more echos here
-mkdir -p ~/FXServer/server
-echo "made directory ~/FXServer/server"
-wget /artifacts/fivem/build_proot_linux/master/5820-8e883dcc7941ce1fa5de359ede53afe587070723/fx.tar.xz
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/5848-4f71128ee48b07026d6d7229a60ebc5f40f2b9db/fx.tar.xz
+
 echo "hit: https://runtime.fivem.net"
-cd ~/FXServer/server && tar xf fx.tar.xz
+tar xf fx.tar.xz
+
 echo "extraction complete"
-git clone https://github.com/citizenfx/cfx-server-data.git ~/FXServer/server-data
+git clone https://github.com/citizenfx/cfx-server-data.git
+
 echo "got server data from deprecated github repo"
-cd ~/FXServer/server-data && bash ~/FXServer/server/run.sh +exec server.cfg
+
+cd ~/$server_name/server-data && bash ~/$server_name/server/run.sh +exec server.cfg
+
 echo "server install worked"
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+echo "removing unnecessary files"
+rm fx.tar.xz
+echo "starting server..."
+./run.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#git clone https://github.com/citizenfx/cfx-server-data.git ~/FXServer/server-data
+#echo "got server data from deprecated github repo"
+#cd ~/FXServer/server-data && bash ~/FXServer/server/run.sh +exec server.cfg
+#echo "server install worked"
+
 
       # i dont think this is right
-      append to server.conf
+      #append to server.conf
       [
       # Only change the IP if you're using a server with multiple network interfaces, otherwise change the port only.
       # this needs to be changed depending on what ports are in use some ports will be reserved 
       # change these ports depending on what ports are in use probably just increment by 1
-      endpoint_add_tcp "0.0.0.0:30120"
-      endpoint_add_udp "0.0.0.0:30120"
+      #endpoint_add_tcp "0.0.0.0:30120"
+      #endpoint_add_udp "0.0.0.0:30120"
 
       # These resources will start by default.
-      ensure mapmanager
-      ensure chat
-      ensure spawnmanager
-      ensure sessionmanager
-      ensure basic-gamemode
-      ensure hardcap
-      ensure rconlog
+      #ensure mapmanager
+      #ensure chat
+      #ensure spawnmanager
+      #ensure sessionmanager
+      #ensure basic-gamemode
+      #ensure hardcap
+      #ensure rconlog
 
       # This allows players to use scripthook-based plugins such as the legacy Lambda Menu.
       # Set this to 1 to allow scripthook. Do note that this does _not_ guarantee players won't be able to use external plugins.
-      sv_scriptHookAllowed 0
+      #sv_scriptHookAllowed 0
 
       # Uncomment this and set a password to enable RCON. Make sure to change the password - it should look like rcon_password "YOURPASSWORD"
       #rcon_password ""
@@ -105,11 +96,11 @@ echo "server install worked"
       # - sets tags "drifting, cars, racing"
       # Or:
       # - sets tags "roleplay, military, tanks"
-      sets tags "default"
+      #sets tags "default"
 
       # A valid locale identifier for your server's primary language.
       # For example "en-US", "fr-CA", "nl-NL", "de-DE", "en-GB", "pt-BR"
-      sets locale "root-AQ" 
+      #sets locale "root-AQ" 
       # please DO replace root-AQ on the line ABOVE with a real language! :)
 
       # Set an optional server info and connecting banner image url.
@@ -118,13 +109,13 @@ echo "server install worked"
       #sets banner_connecting "https://url.to/image.png"
 
       # Set your server's hostname. This is not usually shown anywhere in listings.
-      sv_hostname "FXServer, but unconfigured"
+      #sv_hostname "FXServer, but unconfigured"
 
       # Set your server's Project Name
-      sets sv_projectName "My FXServer Project"
+      #sets sv_projectName "My FXServer Project"
 
       # Set your server's Project Description
-      sets sv_projectDesc "Default FXServer requiring configuration"
+      #sets sv_projectDesc "Default FXServer requiring configuration"
 
       # Nested configs!
       #exec server_internal.cfg
@@ -133,7 +124,7 @@ echo "server install worked"
       #load_server_icon myLogo.png
 
       # convars which can be used in scripts
-      set temp_convar "hey world!"
+      #set temp_convar "hey world!"
 
       # Remove the `#` from the below line if you want your server to be listed as 'private' in the server browser.
       # Do not edit it if you *do not* want your server listed as 'private'.
@@ -142,22 +133,22 @@ echo "server install worked"
       #sv_master1 ""
 
       # Add system admins
-      add_ace group.admin command allow # allow all commands
-      add_ace group.admin command.quit deny # but don't allow quit
-      add_principal identifier.fivem:1 group.admin # add the admin to the group
+      #add_ace group.admin command allow # allow all commands
+      #add_ace group.admin command.quit deny # but don't allow quit
+      #add_principal identifier.fivem:1 group.admin # add the admin to the group
 
       # enable OneSync (required for server-side state awareness)
-      set onesync on
+      #set onesync on
 
       # Server player slot limit (see https://fivem.net/server-hosting for limits)
-      sv_maxclients 48
+      #sv_maxclients 48
 
       # Steam Web API key, if you want to use Steam authentication (https://steamcommunity.com/dev/apikey)
       # -> replace "" with the key
-      set steam_webApiKey ""
+      #set steam_webApiKey ""
 
       # License key for your server (https://keymaster.fivem.net)
       # license key needs to be changed depending on what the user enters 
-      sv_licenseKey changeme
-      ]
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+      #sv_licenseKey changeme
+      #]
+
