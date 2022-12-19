3a993e6fabe84ace530a540b1dff0ea1b104e9eb
philip12845 <87152429+philip12845@users.noreply.github.com>
2022-12-19 20:53:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade 
 echo "updated and upgraded all packages"
 
-sudo apt install git && sudo apt install xz-utils
+sudo apt install git && sudo apt install xz-utils 
 echo "installed dependencies (git and xz-utils)"
 
 # note to self: look at expect program for linux
 
 echo "welcome to the fivem linux installer..."
 
-echo -n "Your server name: "
+echo -n "Your server name:"
 read server_name
 mkdir "$server_name"
 echo "madedir $server_name"
 cd $server_name
 echo "cd into $server_name"
 
-echo -n "Your name: "
+echo -n "Your name:"
 read user_name
 mkdir "$user_name"
 echo "madedir"
 cd $user_name
 echo "cd into $username"
+
+ls -l /usr/error 2> ls-error-file.txt
+
 
 echo "start of server install"
 wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/5848-4f71128ee48b07026d6d7229a60ebc5f40f2b9db/fx.tar.xz
@@ -31,21 +34,22 @@ echo "hit: https://runtime.fivem.net"
 tar xf fx.tar.xz
 
 echo "extraction complete"
-git clone https://github.com/citizenfx/cfx-server-data.git
-
-echo "got server data from deprecated github repo"
+git clone https://github.com/citizenfx/cfx-server-data.git 
+echo "got server data"
 
 cd ~/$server_name/server-data && bash ~/$server_name/server/run.sh +exec server.cfg
-
 echo "server install worked"
+
+
 
 echo "removing unnecessary files"
 rm fx.tar.xz
 echo "starting server..."
 ./run.sh
 
-# might need to have a look at xdotool look at standard error to make error log for this script 
-#git clone https://github.com/citizenfx/cfx-server-data.git ~/FXServer/server-data
-#echo "got server data from deprecated github repo"
+look at standard error to make error log for this script 
 #cd ~/FXServer/server-data && bash ~/FXServer/server/run.sh +exec server.cfg
-#echo "server install worked"    
+   
+
+
+
